5e63e77a98f991759d950b3ca20a2999ce51f947
varunswing <varuns939@gmail.com>
2020-09-17 10:59:08 +0530

    Update count_triplets.cpp

--- arrays/count_triplets.cpp
+++ arrays/count_triplets.cpp
@@ -6,43 +6,30 @@ int main(){
 	cin >> t;
 	while(t--){
 		cin >> n;
-		int arr[n], max_val = 0, ans = 0;
+		int arr[n], ans = 0;
 		for(i=0; i<n; i++){
 			cin >> arr[i];
 		};
-		for(i=0; i<n; i++){
-			max_val = max(max_val, arr[i]);
-		}
-		int freq[max_val + 1] = {0};
-		for(i=0; i<n; i++){
-			freq[arr[i]]++;
-		}
+		
+		sort(arr, arr+n);
 
-		for(i=0; i<n; i++){
-			ans += freq[i] * (freq[i] - 1) * (freq[i] - 2) / 6;
-		}
-
-		for (int i = 1; i <= max_val; i++)  {
-            ans += freq[0] * freq[i] * (freq[i] - 1) / 2; 
-		}
-
-		for(i=0; i<n; i++){
-			if(2*i <= max_val){
-				ans += freq[i] * (freq[i] - 1) / 2 * freq[2*i];
+		for(i=n-1; i>=0; i--){
+			int j = 0;
+			int k = i-1;
+			while(k > j){
+				if(arr[i] == arr[j] + arr[k]){
+					ans ++;
+					j++;
+					k--;
+				}else if(arr[i] > arr[j] + arr[k]){
+					j++;
+				}else{
+					k--;
+				}
 			}
 		}
 
-		for(i=0; i<n; i++){
-			for(int j=i+1; j+i <= max_val; j++){
-				ans += freq[i] * freq[j] * freq[i + j];
-			}
-		}
-
-		if(ans == 0){
-			cout << -1 << endl;
-		}else{
-			cout << ans << endl;
-		}
+		cout << ans << endl;
 	}
 
 	return 0;
