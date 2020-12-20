cb6d4fced35fe38123a5f9d4cff3d18681fe5fbf
varunswing <varuns939@gmail.com>
2020-12-20 23:57:57 +0530

    coding

---
+++ bit magic/check_kth_bit.cpp
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+//User function Template for C++
+
+// Function to check if Kth bit is set or not
+bool checkKthBit(int n, int k){
+    
+    // Your code here
+    // It can be a one liner logic!! Think of it!!
+    if(n & (1 << k)){
+        return true;
+    }else{
+        return false;
+    }
+    
+}
+
+// { Driver Code Starts.
+
+// Driver Code
+int main()
+{
+	int t;
+	cin>>t;//taking testcases
+	while(t--)
+	{
+	    long long n;
+	    cin>>n;//input n
+	    int k;
+	    cin>>k;//bit number k
+	    
+	    if(checkKthBit(n, k))
+	        cout << "Yes" << endl;
+	    else
+	        cout << "No" << endl;
+	}
+	return 0;
+}  // } Driver Code Ends
--- bit magic/powerOf2.cpp
+++ bit magic/powerOf2.cpp
@@ -1,22 +1,53 @@
-#include <bits/stdc++.h>
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
 using namespace std;
-#define bool int 
- 
-bool isPowerOfTwo (long long int x) 
-{ 
-    return x && (!(x&(x-1))); 
-} 
- 
-/*Driver code*/
-int main() 
-{ 
+
+
+ // } Driver Code Ends
+
+
+//User function Template for C++
+
+// Function to check power of two
+bool isPowerofTwo(long long n){
+    
+    // Your code here   
+    if((n & n - 1) == 0){
+        return true;
+    }else {
+        return false;
+    }
+    
+}
+
+// { Driver Code Starts.
+
+// Driver code
+int main()
+{
+
     int t;
-	cin >> t;
-	while(t--){
-	    long long int n;
-	    cin >> n;
-	    isPowerOfTwo(n)? cout<<"YES\n": cout<<"NO\n"; 
-	}
-    return 0; 
-} 
- 
+    cin>>t;//testcases
+
+    for(int i=0;i<t;i++)
+    {
+        long long n; //input a number n
+        cin>>n;
+        if(n<=0) // if n is less than equal to zero then it can't be a power of 2
+        {
+            cout<<"NO"<<endl; //so we print No
+            continue; //continue and check for other values
+        }
+
+         if(isPowerofTwo(n))//Now, if log2 produces an integer not decimal then we are sure raising 2 to this value
+             cout<<"YES"<<endl;
+         else
+            cout<<"NO"<<endl;
+
+    }
+
+    return 0;
+}
+  // } Driver Code Ends
