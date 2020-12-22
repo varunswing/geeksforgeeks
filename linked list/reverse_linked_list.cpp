b41acb7eba6459855a13a340de94f5c39c2ab270
varunswing <varuns939@gmail.com>
2020-12-23 00:52:54 +0530

    Create reverse_linked_list.cpp

---
+++ linked list/reverse_linked_list.cpp
@@ -0,0 +1,101 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+/* Function to get the middle of the linked list*/
+struct Node *reverseList(struct Node *head);
+
+void printList(struct Node *head)
+{
+    struct Node *temp = head;
+    while (temp != NULL)
+    {
+       printf("%d ", temp->data);
+       temp  = temp->next;
+    }
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int T,n,l,firstdata;
+    cin>>T;
+
+    while(T--)
+    {
+        struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n;
+        
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        
+        for (int i=1; i<n; i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+        
+        head = reverseList(head);
+        
+        printList(head);
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/* Linked List Node structure:
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}
+
+*/
+
+// Should reverse list and return new head.
+struct Node* reverseList(struct Node *head)
+{
+    // code here
+    // return head of reversed list
+    struct Node* temp = head;
+    struct Node* curr = head; 
+    struct Node* prev = head;
+    if(temp->next == NULL){
+        return head;
+    }else{
+        while(temp->next){
+            curr = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = curr;
+        }
+        head->next = NULL;
+        head = temp;
+        head->next = prev;
+        return head;
+    }
+}
