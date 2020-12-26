37e7b48e597e5f47979d81ea9ec7cbee7f7fb01f
varunswing <varuns939@gmail.com>
2020-12-26 22:00:34 +0530

    Create bfs.cpp

---
+++ graph/bfs.cpp
@@ -0,0 +1,58 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector <int> bfs(vector<int> g[], int N);
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N, E;
+        cin >> N >> E;
+        vector<int> adj[N];
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+        vector <int> res = bfs(adj, N);
+        for (int i = 0; i < res.size (); i++) cout << res[i] << " ";
+        cout << endl;
+    }
+}// } Driver Code Ends
+
+
+/* You have to complete this function*/
+
+/* Function to do BFS of graph
+*  g[]: adj list of the graph
+*  N : number of vertices
+*/
+vector <int> bfs(vector<int> g[], int N) {
+
+    // Your code here
+    queue<int> queue;
+    vector<int> ans;
+    vector<bool> visited(N);
+    
+    if(N){
+        queue.push(0);
+    }
+    while(queue.size()){
+        int node = queue.front();
+        queue.pop();
+        if(visited[node]){
+            continue;
+        }
+        visited[node] = true;
+        ans.push_back(node);
+        for(int i=g[node].size()-1; i>=0; i--){
+            if(!visited[g[node][i]]){
+                queue.push(g[node][i]);
+            }
+        }
+    }
+    return ans;
+}
