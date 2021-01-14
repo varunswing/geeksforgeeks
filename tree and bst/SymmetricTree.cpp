409805ade3d27b05d2ae00f142ab1ba520f989f1
varunswing <varuns939@gmail.com>
2021-01-14 19:03:10 +0530

    Create SymmetricTree.cpp

---
+++ tree and bst/SymmetricTree.cpp
@@ -0,0 +1,137 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+bool isSymmetric(struct Node* root);
+
+int main()
+{
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+		getline(cin,s);
+		Node* root = buildTree(s);
+        if(isSymmetric(root))
+            cout<<"True"<<endl;
+        else 
+            cout<<"False"<<endl;
+  }
+  return 0;
+}
+// } Driver Code Ends
+
+
+/*
+Structure of the node of the tree is as
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+// return true/false denoting whether the tree is Symmetric or not
+bool comp(struct Node* l, struct Node* r){
+    if(!l && !r){
+        return true;
+    }else if(!l || !r){
+        return false;
+    }
+    if(l->data == r->data){
+	    return comp(l->left, r->right) && comp(l->right, r->left);
+	}else{
+	    return false;
+	}
+    
+}
+bool isSymmetric(struct Node* root)
+{
+	// Code here
+	if(!root){
+	    return true;
+	}
+	if(root->right && root->left){
+	    return comp(root->left, root->right);
+	}else{
+	    return false;
+	}
+	
+}
