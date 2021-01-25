fd826bb97403d390ae5129528f3b302d7de95c9f
varunswing <varuns939@gmail.com>
2021-01-25 16:10:35 +0530

    Update kadaneAlgorithm.cpp

--- arrays/kadaneAlgorithm.cpp
+++ arrays/kadaneAlgorithm.cpp
@@ -1,30 +1,48 @@
-#include <bits/stdc++.h>
+// { Driver Code Starts
+#include<bits/stdc++.h>
 using namespace std;
+
+
+ // } Driver Code Ends
+
+
+// Function to find subarray with maximum sum
+// arr: input array
+// n: size of array
+int maxSubarraySum(int arr[], int n){
+    
+    // Your code here
+    int sum=0, ans = 0, j = 0;
+    for(int i=0; i<n; i++){
+        sum += arr[i];
+        if(sum < 0){
+            sum = 0;
+        }
+        if(ans < sum){
+            ans = sum;
+        }
+    }
+    return ans;
+}
+
+// { Driver Code Starts.
 
 int main()
 {
-	//code
-    int t, n;
-    cin >> t;
-    while(t--){
-        int sum = 0;
-        cin >> n;
-        int arr[n];
-        for(int i=0; i<n; i++){
-            cin >> arr[i];
-        }
-        int max_val = arr[0];
-        for(int i=0; i<n-1; i++){
-            sum = arr[i];
-            max_val = max(sum, max_val);
-            for(int j=i+1; j<n; j++){
-                sum += arr[j];
-                max_val = max(sum, max_val);
-            }
-        }
-
-        cout << max_val << endl;
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+        
+        cout << maxSubarraySum(a, n) << endl;
     }
-
-	return 0;
 }
+  // } Driver Code Ends
