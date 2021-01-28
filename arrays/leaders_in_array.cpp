e57bc8de5b167029e247859314cea5760de5c72d
varunswing <varuns939@gmail.com>
2021-01-28 22:23:06 +0530

    Update leaders_in_array.cpp

--- arrays/leaders_in_array.cpp
+++ arrays/leaders_in_array.cpp
@@ -1,32 +1,57 @@
+// { Driver Code Starts
+// C++ program to remove recurring digits from
+// a given number
 #include <bits/stdc++.h>
 using namespace std;
 
+
+ // } Driver Code Ends
+
+
+
+
+vector<int> leaders(int a[], int n){
+    // code here
+    int max = INT_MIN;
+    vector<int> ans;
+    for(int i=n-1; i>=0; i--){
+        if(max <= a[i]){
+            max = a[i];
+            ans.push_back(max);
+        }
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+// { Driver Code Starts.
+
 int main()
- {
-	//code
-    int t;
-    cin >> t;
-    while(t--){
-        int n;
-        cin >> n;
+{
+   long long t;
+   cin >> t;//testcases
+   while (t--)
+   {
+       long long n;
+       cin >> n;//total size of array
+        
         int a[n];
-        stack<int> st;
-        for(int i=0; i<n; i++){
+        
+        //inserting elements in the array
+        for(long long i =0;i<n;i++){
             cin >> a[i];
         }
-        st.push(a[n-1]);
-        for(int i=n-2; i>=0; i--){
-            if(st.top() <= a[i]){
-                st.push(a[i]);
-            }
-        }
-        while(st.empty() == false){
-            cout << st.top() << " ";
-            st.pop();
-        }
-        cout << endl;
         
-    }
+        //calling leaders() function
+        vector<int> v = leaders(a, n);
+        
+        //printing elements of the vector
+        for(auto it = v.begin();it!=v.end();it++){
+            cout << *it << " ";
+        }
+        
+        cout << endl;
 
-	return 0;
+   }
 }
+  // } Driver Code Ends
