fbb8d73bc1991ed84d071ced23c5141e8e1edfa9
varunswing <varuns939@gmail.com>
2021-01-28 23:03:37 +0530

    Create ReverseArrayInGroups.cpp

---
+++ arrays/ReverseArrayInGroups.cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+//User function template for C++
+
+class Solution{
+public:
+    void reverseInGroups(vector<long long>& arr, int n, int k){
+        // code here
+        int i;
+        for(i=0; i < n-k ; i+=k){
+            reverse(arr.begin()+i, arr.begin()+i+k);
+        }
+        reverse(arr.begin()+i , arr.begin()+n);
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t; 
+    cin >> t; 
+    while(t--){ 
+        int n;
+        cin >> n; 
+        vector<long long> arr; 
+        int k;
+        cin >> k; 
+
+        for(long long i = 0; i<n; i++)
+        {
+            long long x;
+            cin >> x; 
+            arr.push_back(x); 
+        }
+        Solution ob;
+        ob.reverseInGroups(arr, n, k);
+        
+        for(long long i = 0; i<n; i++){
+            cout << arr[i] << " "; 
+        }
+        cout << endl;
+    }
+}
+
+  // } Driver Code Ends
