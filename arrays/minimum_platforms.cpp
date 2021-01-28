93cd99accf4b20980446eef89d3d130b8c7c24b3
varunswing <varuns939@gmail.com>
2021-01-28 22:23:17 +0530

    Update minimum_platforms.cpp

--- arrays/minimum_platforms.cpp
+++ arrays/minimum_platforms.cpp
@@ -1,53 +1,56 @@
-#include<iostream>
-#include<bits/stdc++.h>
+// { Driver Code Starts
+// Program to find minimum number of platforms
+// required on a railway station
+#include <bits/stdc++.h>
 using namespace std;
-int minimumPlatforms(int *a,int *d,int n)
+
+
+ // } Driver Code Ends
+
+
+int findPlatform(int arr[], int dep[], int n)
 {
-    multimap<int,int> m;
-    for(int i=0;i<n;i++)
-    {
-        m.insert({a[i],d[i]});
-    }
-    int k=0;
-    for(auto i=m.begin();i!=m.end();i++)
-    {
-        a[k]=i->first;
-        d[k]=i->second;
-        k++;
-    }
-    /*sort(a,a+n);
-    sort(d,d+n);* Execution Time : 0.11 */
-    int pf=1,c,j;
-    for(int i=1;i<n;i++)
-    {
-        j=0,c=1;
-        while(j<i)
-        {
-            if(a[j]<=a[i]&&a[i]<=d[j]) c++;
-            j++;
-        }
-        if(pf<c) pf = c;
-    }
-    return pf;
-}
-int main()
- {
-	int t;
-	cin>>t;
-	while(t--)
-	{
-	    int n;
-	    cin>>n;
-	    int a[n],d[n];
-	    for(int i=0;i<n;i++)
-	    {
-	        cin>>a[i];
+	// Your code here
+	sort(arr, arr+n);
+	sort(dep, dep+n);
+	int i = 1;
+	int j = 0;
+	int p = 1;
+	int max = 1;
+	while(i<n && j<n){
+	    if(arr[i] > dep[j]){
+	        p--;
+	        j++;
+	    }else if(arr[i] <= dep[j]){
+	        p++;
+	        i++;
+    	    if(p > max){
+                max = p;
+            }
 	    }
-	    for(int j=0;j<n;j++)
-	    {
-	        cin>>d[j];
-	    }
-	    cout<<minimumPlatforms(a,d,n)<<endl;
 	}
-	return 0;
+	return max;
 }
+
+
+// { Driver Code Starts.
+// Driver code
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        int dep[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        for(int j=0;j<n;j++){
+            cin>>dep[j];
+        }
+    cout <<findPlatform(arr, dep, n)<<endl;
+    } 
+   return 0;
+}  // } Driver Code Ends
