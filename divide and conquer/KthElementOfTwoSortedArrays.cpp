8484718d8d6d65402009801d0a86960120dfb0f2
varunswing <varuns939@gmail.com>
2021-03-30 11:45:56 +0530

    Create KthElementOfTwoSortedArrays.cpp

---
+++ divide and conquer/KthElementOfTwoSortedArrays.cpp
@@ -0,0 +1,39 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    int kthElement(int arr1[], int arr2[], int n, int m, int k)
+    {
+        int a[n+m];
+        
+        merge(arr1, arr1+n, arr2, arr2+m, a);
+        
+        return a[k-1];
+    }
+};
+
+// { Driver Code Starts.
+ 
+// Driver code
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+		int n,m,k;
+		cin>>n>>m>>k;
+		int arr1[n],arr2[m];
+		for(int i=0;i<n;i++)
+			cin>>arr1[i];
+		for(int i=0;i<m;i++)
+			cin>>arr2[i];
+		
+		Solution ob;
+        cout << ob.kthElement(arr1, arr2, n, m, k)<<endl;
+	}
+    return 0;
+}  // } Driver Code Ends
