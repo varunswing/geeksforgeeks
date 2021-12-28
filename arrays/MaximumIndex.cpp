3a26a2e927839e0a58dc7e502a17a79b46d566e2
varunswing <varuns939@gmail.com>
2021-12-28 23:36:53 +0530

    Create MaximumIndex.cpp

---
+++ arrays/MaximumIndex.cpp
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+        
+    // A[]: input array
+    // N: size of array
+    // Function to find the maximum index difference.
+    int maxIndexDiff(int A[], int N) 
+    { 
+        // Your code here
+        int max = 0;
+        for(int i=0; i<N; i++){
+            for(int j=N-1; j>=i+max; j--){
+                if(A[j] >= A[i]){
+                    if(j-i > max){
+                        max = j-i;
+                    }
+                }
+            }
+        
+        }
+        return max;
+    }
+};
+
+// { Driver Code Starts.
+  
+/* Driver program to test above functions */
+int main() 
+{
+    int T;
+    //testcases
+    cin>>T;
+    while(T--){
+        int num;
+        //size of array
+        cin>>num;
+        int arr[num];
+        
+        //inserting elements
+        for (int i = 0; i<num; i++)
+            cin>>arr[i];
+        Solution ob;
+        
+        //calling maxIndexDiff() function
+        cout<<ob.maxIndexDiff(arr, num)<<endl;    
+        
+    }
+    return 0;
+}   // } Driver Code Ends
